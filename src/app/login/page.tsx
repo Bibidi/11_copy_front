e2b4225bea1e0e6cabeae2873da9b4229d90e7dd
Bibidi <byungdeuk.kim@gmail.com>
2024-03-16 00:03:32 +0900

    로그인 컴포넌트 위치 조정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,36 +3,38 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function LoginPage() {
   return (
-    <form className={styles.loginContainer} action="" method="POST">
-      <div className="mb-3">
-        <label className="form-check-label" htmlFor="username">Email</label>
-        <input
-          className="form-control"
-          type="text"
-          id="username"
-          name="username"
-          required
-        />
-      </div>
-      <div className="mb-3">
-        <label className="form-check-label" htmlFor="password">Password</label>
-        <input
-          className="form-control"
-          type="password"
-          id="password"
-          name="password"
-          required
-        />
-      </div>
-      <div className="mb-3 form-check">
-        <input
-          className="form-check-input"
-          type="checkbox"
-          id="remember"
-        />
-        <label className="form-check-label" htmlFor="remember">Remember</label>
-      </div>
-      <button className="btn btn-primary" type="submit">로그인</button>
-    </form>
+    <div className={styles.loginContainer}>
+      <form className={styles.loginForm} action="" method="POST">
+        <div className="mb-3">
+          <label className="form-check-label" htmlFor="username">Email</label>
+          <input
+            className="form-control"
+            type="text"
+            id="username"
+            name="username"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-check-label" htmlFor="password">Password</label>
+          <input
+            className="form-control"
+            type="password"
+            id="password"
+            name="password"
+            required
+          />
+        </div>
+        <div className="mb-3 form-check">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            id="remember"
+          />
+          <label className="form-check-label" htmlFor="remember">Remember</label>
+        </div>
+        <button className="btn btn-primary" type="submit">로그인</button>
+      </form>
+    </div>
   )
 }
